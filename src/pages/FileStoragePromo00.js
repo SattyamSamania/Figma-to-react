8f0664770738bc433f257edc1b132cd8d6866a79
SattyamSamania <SattyamSamania>
2023-10-31 13:38:14 +0000

    Pushed from Locofy Builder

---
+++ src/pages/FileStoragePromo00.js
@@ -0,0 +1,7 @@
+const FileStoragePromo00 = () => {
+  return (
+    <div className="relative w-full h-[1200px] bg-[url('/public/file-storagepromo00@3x.png')] bg-cover bg-no-repeat bg-[top]" />
+  );
+};
+
+export default FileStoragePromo00;
